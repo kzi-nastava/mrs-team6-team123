e8690d17b53c7bc0190e24e9dc272e10722644a8
aleksandar-aleksandar <apapic256@gmail.com>
2025-12-18 00:37:29 +0100

    Simple routing

--- client-layer/src/app/app.routes.ts
+++ client-layer/src/app/app.routes.ts
@@ -1,4 +1,45 @@
 import { Routes } from '@angular/router';
 
-export const routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home').then(m => m.HomeComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login').then(m => m.LoginComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register/register').then(m => m.RegisterComponent)
+  },
+  {
+    path: 'history',
+    loadComponent: () =>
+      import('./pages/history/history').then(m => m.HistoryComponent)
+  },
+  {
+    path: 'favorites',
+    loadComponent: () =>
+      import('./pages/favorites/favorites').then(m => m.FavoritesComponent)
+  },
+  {
+    path: 'notifications',
+    loadComponent: () =>
+      import('./pages/notifications/notifications').then(m => m.NotificationsComponent)
+  },
+  {
+    path: 'profile',
+    loadComponent: () =>
+      import('./pages/profile/profile').then(m => m.ProfileComponent)
+  },
 
+  // fallback
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
