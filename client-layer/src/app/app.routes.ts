1bb365357554c4c6e0a112d5ee12a15b1c424e9b
Ana Paroski <143261039+anaparoskii@users.noreply.github.com>
2025-12-19 22:53:26 +0100

    Implement ride history page for driver
    - added ride history filter and table shared components
    
    - added ride history page component for driver

--- client-layer/src/app/app.routes.ts
+++ client-layer/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RideHistoryComponent } from './pages/driver/ride-history/ride-history';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'driver/ride-history', component: RideHistoryComponent }
+];
