076a28b3271de7ba75a117c38535c2d6f54337b4
llanabanana <mirkov.sv23.2023@uns.ac.rs>
2025-12-20 21:58:48 +0100

    Add registered home page with map component and routing updates
    - Implemented RegisteredHome component with map integration.
    - Added styles for registered home and map components
    - Refactored navbar links for better readability.
    - Added unit tests for new components.

--- client-layer/src/app/app.ts
+++ client-layer/src/app/app.ts
@@ -10,4 +10,12 @@ import { NavbarComponent } from './components/navbar/navbar';
 })
 export class App {
   protected readonly title = signal('client-layer');
+  
+  registeredUserLinks = [
+    { label: 'Home', route: '/registered-home' },
+    { label: 'History', route: '/history' },
+    { label: 'Favorites', route: '/favorites' },
+    { label: 'Notifications', route: '/notifications' },
+    { label: 'Profile', route: '/profile' }
+  ];
 }
---
+++ client-layer/src/app/registered-home/registered-home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MapComponent } from '../map/map';
+
+@Component({
+  selector: 'app-registered-home',
+  imports: [MapComponent],
+  templateUrl: './registered-home.html',
+  styleUrl: './registered-home.css',
+})
+export class RegisteredHome {
+}
