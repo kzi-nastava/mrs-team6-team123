2b085a5c4a01ea7a053665e8fd166975180f5fc0
llanabanana <mirkov.sv23.2023@uns.ac.rs>
2025-12-22 18:33:38 +0100

    Add user favorites feature with route card display and styling

--- client-layer/src/app/app.routes.ts
+++ client-layer/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { RideHistoryComponent } from './pages/driver/ride-history/ride-history';
 import { RegisteredHome } from './pages/registered-home/registered-home';
 import { UnregisteredHomeComponent } from './pages/unregistered-home/unregistered-home';
+import { UserFavoritesComponent } from './pages/user-favorites/user-favorites';
 
 export const routes: Routes = [
   {path: 'driver/ride-history', component: RideHistoryComponent },
 
   { path: '', redirectTo: '/unregistered-home', pathMatch: 'full' },
   { path: 'registered-home', component: RegisteredHome },
+  { path: 'favorites', component: UserFavoritesComponent },
 
   { path: 'unregistered-home', component: UnregisteredHomeComponent }
 ];
