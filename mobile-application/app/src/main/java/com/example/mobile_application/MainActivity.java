43e0a0decb0488a3845af131370c527a680c9835
Ana Paroski <143261039+anaparoskii@users.noreply.github.com>
2025-12-28 19:27:15 +0100

    Add navbar and toolbar
    - TODO: fix navbar links and toolbar position

--- mobile-application/app/src/main/java/com/example/mobile_application/MainActivity.java
+++ mobile-application/app/src/main/java/com/example/mobile_application/MainActivity.java
@@ -1,24 +1,61 @@
 package com.example.mobile_application;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import com.example.mobile_application.map.MapFragment;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+    private NavigationView drawerMenuView;
+    private BottomNavigationView bottomNavigationView;
+    private MaterialToolbar toolbarView;
+
+    private boolean isLoggedIn = true;
+    private String userRole = "user"; // "driver" | "admin"
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        drawerMenuView = findViewById(R.id.navigation_view);
+        bottomNavigationView = findViewById(R.id.navbar);
+        toolbarView = findViewById(R.id.main_toolbar);
+
+        findViewById(R.id.nav_hamburger).setOnClickListener(v ->
+                drawerLayout.openDrawer(GravityCompat.END)
+        );
+
+        if (!isLoggedIn) {
+            Menu menu = bottomNavigationView.getMenu();
+            menu.findItem(R.id.nav_hamburger).setVisible(false);
+        }
+
+        if (saveInstanceState == null) {
+            loadFragment(new MapFragment());
+        }
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_container, fragment)
+                .commit();
     }
 }
