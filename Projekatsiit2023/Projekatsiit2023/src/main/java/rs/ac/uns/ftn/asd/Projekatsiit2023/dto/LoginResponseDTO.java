9ff92674165d5288aebb45ac97892128af1a9655
aleksandar-aleksandar <apapic256@gmail.com>
2025-12-28 16:21:20 +0100

    ISS KT1

---
+++ Projekatsiit2023/Projekatsiit2023/src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2023/dto/LoginResponseDTO.java
@@ -0,0 +1,42 @@
+package rs.ac.uns.ftn.asd.Projekatsiit2023.dto;
+
+public class LoginResponseDTO {
+    private String token;
+    private Long userId;
+    private String email;
+    private String role;
+
+    public LoginResponseDTO() {}
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
