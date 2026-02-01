dbeb65a5a675278e589b30556a16d5ceb34da2cb
Ana Paroski <143261039+anaparoskii@users.noreply.github.com>
2026-02-01 22:43:17 +0100

    Add ride status feild to RideInfoDTO
    - used to disable/enable finish ride button

--- Projekatsiit2023/Projekatsiit2023/src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2023/dtos/ride/RideInfoDTO.java
+++ Projekatsiit2023/Projekatsiit2023/src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2023/dtos/ride/RideInfoDTO.java
@@ -3,6 +3,7 @@ package rs.ac.uns.ftn.asd.Projekatsiit2023.dtos.ride;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import rs.ac.uns.ftn.asd.Projekatsiit2023.enums.RideStatus;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,4 +20,5 @@ public class RideInfoDTO {
     private int duration;
     private List<String> passengers = new ArrayList<>();
     private List<String> reports = new ArrayList<>();
+    private RideStatus status;
 }
