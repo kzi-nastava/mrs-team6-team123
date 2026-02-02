89dd95b3a5e70a4e7801a9b9dae68caab7e9932f
Ana Paroski <143261039+anaparoskii@users.noreply.github.com>
2026-02-02 14:17:04 +0100

    Added notification panel
    - panel with two tabs: unread and read
    
    - fixed navbar to open panel instead of page with route

---
+++ client-layer/src/app/components/notification-panel/notification-panel.ts
@@ -0,0 +1,83 @@
+import { Component } from '@angular/core';
+import { NotificationResponse } from '../../models/notification.model';
+import { NotificationService } from '../../services/notification.service';
+import { AuthService } from '../../services/auth.service';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-notification-panel',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './notification-panel.html',
+  styleUrls: ['./notification-panel.css'],
+})
+export class NotificationPanelComponent {
+  activeTab: 'UNREAD' | 'READ' = 'UNREAD';
+
+  unreadNotifications: NotificationResponse[] = [];
+  readNotifications: NotificationResponse[] = [];
+
+  private sub?: Subscription;
+  private userId!: number;
+
+  loading = false;
+
+  constructor(
+    private notificationService: NotificationService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.userId = this.authService.getCurrentUserId()!;
+    this.loadUnread();
+  }
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+  }
+
+  switchTab(tab: 'UNREAD' | 'READ') {
+    this.activeTab = tab;
+
+    if (tab === 'UNREAD') {
+      this.loadUnread();
+    } else {
+      this.loadRead();
+    }
+  }
+
+  private loadUnread() {
+    this.loading = true;
+
+    this.notificationService.loadUnread(this.userId);
+
+    this.sub = this.notificationService
+      .getUnread()
+      .subscribe((list: NotificationResponse[]) => {
+        this.unreadNotifications = list;
+        this.loading = false;
+      });
+  }
+
+  private loadRead() {
+    this.loading = true;
+
+    this.notificationService
+      .getRead(this.userId)
+      .subscribe((list: NotificationResponse[]) => {
+        this.readNotifications = list;
+        this.loading = false;
+      });
+  }
+
+  markAsRead(notification: NotificationResponse) {
+    this.notificationService.markAsRead(notification.notificationId).subscribe(() => {
+      this.unreadNotifications =
+        this.unreadNotifications.filter(n => n.notificationId !== notification.notificationId);
+
+      this.readNotifications.unshift(notification);
+    });
+  }
+}
