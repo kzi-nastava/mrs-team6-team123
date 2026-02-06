afc6580630680771c2c03affbc4956ec23d54fc7
Ana Paroski <143261039+anaparoskii@users.noreply.github.com>
2026-02-07 00:31:11 +0100

    Design track ride page and add necessary endpoints and services
    - TODO: load data from backend and display it based on user role
    
    - TODO: implement on click listeners for passenger buttons
    
    - TODO: display route on map and rides vehicle

---
+++ mobile-application/app/src/main/java/com/example/mobile_application/ui/track_ride/TrackRideFragment.java
@@ -0,0 +1,21 @@
+package com.example.mobile_application.ui.track_ride;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mobile_application.R;
+
+public class TrackRideFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_track_ride, container, false);
+
+        return view;
+    }
+}
