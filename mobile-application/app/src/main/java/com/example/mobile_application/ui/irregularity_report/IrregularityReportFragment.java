ff5fe1271351aa2d5d3f9f4a85b52e7524aac5b6
Ana Paroski <143261039+anaparoskii@users.noreply.github.com>
2026-02-09 00:13:04 +0100

    Added irregularity report fragment
    - implemented loading data and cancel button
    
    - TODO: implement sending report to backend
    
    - TODO: open fragment from track ride page

--- mobile-application/app/src/main/java/com/example/mobile_application/ui/irregularity_report/IrregularityReportFragment.java
+++ mobile-application/app/src/main/java/com/example/mobile_application/ui/irregularity_report/IrregularityReportFragment.java
@@ -7,60 +7,77 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mobile_application.R;
+import com.example.mobile_application.dto.DriverRideHistoryDTO;
+import com.example.mobile_application.dto.TrackRideDTO;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link IrregularityReportFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.Serializable;
+
 public class IrregularityReportFragment extends Fragment {
+    private static final String ARG_RIDE = "ride";
+    private TextView tvDriver;
+    private EditText etComment;
+    private Button btnReport, btnCancel;
+    private TrackRideDTO ride;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public IrregularityReportFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment IrregularityReportFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static IrregularityReportFragment newInstance(String param1, String param2) {
+    public static IrregularityReportFragment newInstance(TrackRideDTO ride) {
         IrregularityReportFragment fragment = new IrregularityReportFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putSerializable(ARG_RIDE, ride);
         fragment.setArguments(args);
         return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_irregularity_report, container, false);
+        View view = inflater.inflate(R.layout.fragment_irregularity_report, container, false);
+
+        tvDriver = view.findViewById(R.id.tvDriver);
+        etComment = view.findViewById(R.id.etComment);
+        btnReport = view.findViewById(R.id.btnReport);
+        btnCancel = view.findViewById(R.id.btnCancel);
+
+        if (getArguments() != null) {
+            ride = (TrackRideDTO) getArguments().getSerializable("ride");
+        }
+
+        dataSetup();
+        btnReport.setOnClickListener(v -> report());
+        btnCancel.setOnClickListener(v -> cancel());
+
+        return view;
+    }
+
+    public void dataSetup() {
+        tvDriver.setText(String.format("Driver: %s", ride.getInfo().getDriver()));
+    }
+
+    public void cancel() {
+        requireActivity().getSupportFragmentManager().popBackStack();
+    }
+
+    public void report() {
+        String comment = etComment.getText().toString().trim();
+
+        if (comment.isEmpty()) {
+            if (isAdded())
+                showToast("Enter a comment");
+            return;
+        }
+
+        // TODO: implement sending report to backend
+
+        requireActivity().getSupportFragmentManager().popBackStack();
+    }
+
+    private void showToast(String message) {
+        requireActivity().runOnUiThread(() ->
+                Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show());
     }
 }
