fe1f51ad03cda45d22375245b0ee7aa116ec6d5e
Ana Paroski <143261039+anaparoskii@users.noreply.github.com>
2026-02-09 13:32:52 +0100

    Fix: role check for drawer menu

--- mobile-application/app/src/main/java/com/example/mobile_application/MainActivity.java
+++ mobile-application/app/src/main/java/com/example/mobile_application/MainActivity.java
@@ -61,13 +61,13 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView.setSelectedItemId(R.id.nav_home);
 
         Menu menu = drawerMenuView.getMenu();
-        if (userRole.equals("admin")) {
+        if (userRole.equals(getString(R.string.role_admin))) {
             menu.findItem(R.id.favorites).setVisible(false);
-        } else if (userRole.equals("driver")) {
+        } else if (userRole.equals(getString(R.string.role_driver))) {
             menu.findItem(R.id.favorites).setVisible(false);
             menu.findItem(R.id.drivers).setVisible(false);
             menu.findItem(R.id.pricing).setVisible(false);
-        } else if (userRole.equals("user")) {
+        } else if (userRole.equals(getString(R.string.role_passenger))) {
             menu.findItem(R.id.drivers).setVisible(false);
             menu.findItem(R.id.pricing).setVisible(false);
         }
