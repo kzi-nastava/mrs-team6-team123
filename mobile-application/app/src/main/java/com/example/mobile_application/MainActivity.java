69f62bfbc4ca526ae61d7dc73143183b96808dc9
Ana Paroski <143261039+anaparoskii@users.noreply.github.com>
2026-02-10 22:10:54 +0100

    Added opening track ride from in-app notifications

--- mobile-application/app/src/main/java/com/example/mobile_application/MainActivity.java
+++ mobile-application/app/src/main/java/com/example/mobile_application/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean isLoggedIn = true; // TODO: Change to false after login is implemented
     private Long testUserId = 3L; // TODO: Get from login authentication
-    private String userRole = "ADMIN"; // "DRIVER" | "PASSENGER"
+    private String userRole = "PASSENGER"; // "DRIVER" | "ADMIN"
 
     @Override
     protected void onCreate(Bundle saveInstanceState) {
@@ -74,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
                 if (userRole.equals(getString(R.string.role_admin)))
                     loadFragment(new AdminHomeFragment());
                 else
-                    loadFragment(TrackRideFragment.newInstance(14L));
+                    loadFragment(new MapFragment());
             }
             else
                 loadFragment(new UnregisteredHomeFragment());
