1f792bcc493619a76aa59fb7a24a6b36ff1534a8
Ana Paroski <143261039+anaparoskii@users.noreply.github.com>
2026-02-10 20:07:08 +0100

    Changed chat UI
    - chat is now a fragment and takes up the whole screen
    
    - chat can be accessed through drawer menu by logged in users
    
    - admin opens chat list fragment with list of all their previous chats
    
    - other users open a consistent chat with one admin

---
+++ mobile-application/app/src/main/java/com/example/mobile_application/ui/chat/ChatListFragment.java
@@ -0,0 +1,19 @@
+package com.example.mobile_application.ui.chat;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mobile_application.R;
+
+public class ChatListFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_chat_list, container, false);
+    }
+}
