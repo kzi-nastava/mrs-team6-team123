696440fbfbb33cd84e0bf8b3249c8ae95d5076e4
Ana Paroski <143261039+anaparoskii@users.noreply.github.com>
2026-02-16 18:33:54 +0100

    Fix driver logout and login
    - at login, inicialize drivers vehicle as active
    
    - at logout, remove drivers vehicle from active vehicles

--- Projekatsiit2023/Projekatsiit2023/src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2023/services/AuthService.java
+++ Projekatsiit2023/Projekatsiit2023/src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2023/services/AuthService.java
@@ -41,6 +41,7 @@ public class AuthService {
     private final DriverRepository driverRepository;
     private final ActiveVehicleRepository activeVehicleRepository;
     private final DriverStatusService driverStatusService;
+    private final PublicMapService publicMapService;
 
     public AuthService(UserRepository userRepository,
             PasswordEncoder passwordEncoder,
@@ -49,7 +50,7 @@ public class AuthService {
             ActivationTokenRepository activationTokenRepository,
             PasswordResetTokenRepository passwordResetTokenRepository,
             DriverRepository driverRepository, ActiveVehicleRepository activeVehicleRepository,
-            DriverStatusService driverStatusService) {
+            DriverStatusService driverStatusService, PublicMapService publicMapService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtTokenProvider = jwtTokenProvider;
@@ -59,6 +60,7 @@ public class AuthService {
         this.driverRepository = driverRepository;
         this.activeVehicleRepository = activeVehicleRepository;
         this.driverStatusService = driverStatusService;
+        this.publicMapService = publicMapService;
     }
 
     public LoginResponseDTO login(LoginRequestDTO request) {
@@ -78,7 +80,21 @@ public class AuthService {
         }
 
         if (user.getUserRole() == UserRole.DRIVER) {
+            Driver driver = driverRepository.findById(user.getId())
+                    .orElseThrow(() -> new RuntimeException("Driver not found"));
             driverStatusService.activateDriverOnLogin(user.getId());
+            Optional<ActiveVehicle> existing =
+                    activeVehicleRepository.findByVehicleId(driver.getVehicle().getId());
+
+            if (existing.isEmpty()) {
+                ActiveVehicle av = new ActiveVehicle();
+                av.setId(driver.getVehicle().getId());
+                av.setVehicle(driver.getVehicle());
+                av.setCurrentLatitude(45.2576);
+                av.setCurrentLongitude(19.8442);
+                av.setAvailable(true);
+                activeVehicleRepository.save(av);
+            }
         }
 
         String token = jwtTokenProvider.generateToken(user);
@@ -159,6 +175,11 @@ public class AuthService {
                 driver.setActiveMinutesLast24h(0);
             }
             driverRepository.save(driver);
+
+            ActiveVehicle av = activeVehicleRepository.findByVehicleId(driver.getVehicle().getId())
+                    .orElseThrow(() -> new RuntimeException("Active vehicle not found for driver"));
+            activeVehicleRepository.delete(av);
+            activeVehicleRepository.flush();
         }
 
         response.setSuccess(true);
